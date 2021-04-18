6a6e66ef8dd1789f27b2287f8419706d5bd5d239
Edward Weymouth <edward.weymouth@murdochs.com>
2021-04-18 17:13:09 -0600

    Add options for the main sorter

--- airport_timezone.py
+++ airport_timezone.py
@@ -1,44 +1,44 @@
 import airportsdata
+import argparse
 import json
+import sys
+import os
 import numpy as np
-# airports = airportsdata.load()
-# airports = airportsdata.load('IATA')  # key is IATA code
+from tqdm import tqdm
 
-result = []
+def main():
+  country_list = ['AR', 'BO', 'BR', 'CL', 'CO', 'EC', 'FK',	'GF', 'GY', 'PY',	'PE', 'SR', 'UY', 'VE', 'US', 'CA'] # South America, US and canada
+  airports_dict = {}
+  airports_list = []
 
-with open('./lab_table/airports_full.json') as json_file:
-  data = json.load(json_file)
+  parser = argparse.ArgumentParser(description='')
+  parser.add_argument('output_path')
+  parser.add_argument('--iata', dest='iata', action='store_const', const='IATA',  help='Loads the dataset with IATA keys')
+  parser.add_argument('--tz', dest='tz', action='store_true',  help='Creates a json file with ICAO => timezone mappings')
+  parser.add_argument('--map-codes', dest='map_codes', action='store_true',  help='Creates a json file with ICAO => IATA')
+  args = parser.parse_args()
 
-result = list(data)
+  airports = airportsdata.load()
+  # airports = airportsdata.load('IATA')  # key is IATA code
+ 
+  #  {'icao': 'YLZI', 'iata': 'LZR', 'name': 'Lizard Island Airport', 'city': '', 'subd': 'Queensland', 'country': 'AU', 'elevation': 70.0, 'lat': -14.6667003632, 'lon': 145.4499969482, 'tz': 'Australia/Brisbane'}
+  for airport in tqdm(airports):
+    current_airport = airports[airport]
+    if current_airport['country'] in country_list and len(current_airport['iata']) > 0:
+      if(tx)
+        airports_dict[current_airport['icao']] = current_airport['tz']
+      elif(map_codes)
+        airports_dict[current_airport['icao']] = current_airport['iata']
+      else:
+        # Full rewrite
+        airports_dict[airport] = current_airport
 
-airports_dict = {}
-# airports_list = []
-country_list = ['AR', 'BO', 'BR', 'CL', 'CO', 'EC', 'FK',	'GF', 'GY', 'PY',	'PE', 'SR', 'UY', 'VE', 'US', 'CA'] # South America, US and canada
+  json_res = json.dumps(airports_dict, indent = 2)
 
-for airport in result:
-  if airport['country_iso2'] in country_list:
-    airports_dict[airport['icao_code']] = airport['timezone']
+  with open(args.output_path, 'w') as text_file:
+    text_file.write(json_res)
 
-json_res = json.dumps(airports_dict, indent = 2)
+  sys.exit()
 
-with open('./lab_table/airport_timezone_2.json', 'w') as text_file:
-  text_file.write(json_res)
-
-# result = []
-# diff = []
-# airport_list = list(airports.keys())
-
-# def addition(n):
-#   return n['iata_code']
-
-# with open('./lab_table/airports_full.json') as json_file:
-#   data = json.load(json_file)
-#   result = map(addition, data)
-
-# airport_full_things = list(result)
-
-# for iata_code in airport_full_things:
-#   if not iata_code in airport_list:
-#     diff.append(iata_code)
-
-# print(diff)
+if __name__ == "__main__":
+  main()
