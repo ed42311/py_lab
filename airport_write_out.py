a94980a2e0682d9ab13f39527c274c8f0a6989fc
Edward Weymouth <edward.weymouth@murdochs.com>
2021-04-24 13:59:28 -0600

    Add name map

--- airport_write_out.py
+++ airport_write_out.py
@@ -7,7 +7,7 @@ import numpy as np
 from tqdm import tqdm
 
 def main():
-  country_list = ['AR', 'BO', 'BR', 'CL', 'CO', 'EC', 'FK',	'GF', 'GY', 'PY',	'PE', 'SR', 'UY', 'VE', 'US', 'CA'] # South America, US and canada
+  country_list = ['AR', 'BO', 'BR', 'CL', 'CO', 'EC', 'FK',	'GF', 'GY', 'PY',	'PE', 'SR', 'UY', 'VE', 'US', 'CA', 'CR'] # South America, US and canada, Costa Rica
   airports_dict = {}
   airports_list = []
 
@@ -15,23 +15,30 @@ def main():
   parser.add_argument('output_path')
   parser.add_argument('--iata', dest='iata', action='store_const', const='IATA',  help='Loads the dataset with IATA keys')
   parser.add_argument('--tz', dest='tz', action='store_true',  help='Creates a json file with ICAO => timezone mappings')
+  parser.add_argument('--name', dest='name', action='store_true',  help='Creates a json file with IATA => timezone mappings')
   parser.add_argument('--map-codes', dest='map_codes', action='store_true',  help='Creates a json file with ICAO => IATA')
+  parser.add_argument('--load-iata', dest='load_iata', action='store_true',  help='Loads the IATA codes as keys')
   args = parser.parse_args()
 
-  airports = airportsdata.load()
-  # airports = airportsdata.load('IATA')  # key is IATA code
+  if args.name or args.load_iata:
+    # key is IATA code
+    airports = airportsdata.load('IATA')
+  else:
+    airports = airportsdata.load()
  
   #  {'icao': 'YLZI', 'iata': 'LZR', 'name': 'Lizard Island Airport', 'city': '', 'subd': 'Queensland', 'country': 'AU', 'elevation': 70.0, 'lat': -14.6667003632, 'lon': 145.4499969482, 'tz': 'Australia/Brisbane'}
-  for airport in tqdm(airports):
-    current_airport = airports[airport]
-    if current_airport['country'] in country_list and len(current_airport['iata']) > 0:
-      if(tx)
+  for airport_code in tqdm(airports):
+    current_airport = airports[airport_code]
+    if current_airport['country'] in country_list and len(current_airport['iata']):
+      if args.tz:
         airports_dict[current_airport['icao']] = current_airport['tz']
-      elif(map_codes)
+      elif args.map_codes:
         airports_dict[current_airport['icao']] = current_airport['iata']
+      elif args.name:
+        airports_dict[airport_code] = current_airport['name']
       else:
         # Full rewrite
-        airports_dict[airport] = current_airport
+        airports_dict[airport_code] = current_airport
 
   json_res = json.dumps(airports_dict, indent = 2)
 
